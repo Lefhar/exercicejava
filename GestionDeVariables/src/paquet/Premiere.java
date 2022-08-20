dee2948d830935324eda81bc98c95e625e6a063a
Lefebvre Harold <73162352+Lefhar@users.noreply.github.com>
2022-08-20 16:46:50 +0200

    exercice java

--- GestionDeVariables/src/paquet/Premiere.java
+++ GestionDeVariables/src/paquet/Premiere.java
@@ -1,5 +1,7 @@
 package paquet;
 
+import semaine.JourDeLaSemaine;
+
 public class Premiere {
 
 	public static void main(String[] args) {
@@ -11,7 +13,7 @@ public class Premiere {
 		    int i2 =8;
 		    System.out.println(i2);
 		    
-		    
+		    //un seul caractére
 			char variables1;
 			char variables2;
 			char variables3;
@@ -19,13 +21,17 @@ public class Premiere {
 			variables1 = 'c';
 			variables2 = '\n';
 			variables3 = '9';
+			 System.out.println(variables1);
+			 System.out.println(variables2);
 			 System.out.println(variables3);
 			double reel1 = 123.6754;
 			double reel2;
 			double reel3;
 			
+			//chaîne de caractére
 			String chaine ="Voici une belle chaîne";
-			String JourDeLaSemaine = "Lundi";
+			JourDeLaSemaine jour = JourDeLaSemaine.Lundi;
+			 System.out.println(jour);
 	}
 
 }
