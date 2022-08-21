45b60b91729d868b8bbe878b52228d88ebb9afba
Lefebvre Harold <73162352+Lefhar@users.noreply.github.com>
2022-08-21 10:53:57 +0200

    exercice java

---
+++ 205-03-initiation-java/src/paquet/ChaineDeCaractere.java
@@ -0,0 +1,21 @@
+package paquet;
+
+public class ChaineDeCaractere {
+
+	public static void main(String[] args) {
+		
+		String s = new String("Valeur de la chaîne");
+		StringBuffer sb = new StringBuffer();
+		
+		System.out.println(s.length());
+		for(int i=s.length()-1;i>=0;i=i-1)
+		{
+			char c = s.charAt(i);
+			
+			sb.append(c);
+			
+		}
+		System.out.println("chaine de caractére à l'envers "+sb.toString());
+	}
+
+}
