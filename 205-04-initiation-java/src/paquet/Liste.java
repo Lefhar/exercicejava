2e80cf358a93ba4dcc64c508b13fb9b758e7ec10
Lefebvre Harold <73162352+Lefhar@users.noreply.github.com>
2022-08-21 16:29:42 +0200

    exercice java

---
+++ 205-04-initiation-java/src/paquet/Liste.java
@@ -0,0 +1,33 @@
+package paquet;
+
+import java.util.Scanner;
+
+public class Liste {
+
+	public static void main(String[] args) {
+
+		int nbrQuestion ;
+		double somme =0;
+		Scanner Question = new Scanner(System.in);
+		System.out.println("Entrer le nombre de note");
+		
+		nbrQuestion = Question.nextInt();
+		
+		double TabNote[]=new double[nbrQuestion];
+		for(int i=0; i<nbrQuestion;i++)
+		{
+			
+			System.out.println("Entrer une note de la liste");
+			TabNote[i]= Question.nextDouble();
+		}
+		
+		
+		for(int i=0;i<TabNote.length;i++)
+		{
+			somme = somme+TabNote[i];
+			System.out.println(TabNote[i]+" ");
+		}
+		System.out.println("la somme des notes est égal à "+somme);
+	}
+
+}
---
+++ 205-04-initiation-java/src/paquet/ListeTableau.java
@@ -0,0 +1,43 @@
+package paquet;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListeTableau {
+
+	public static void main(String[] args) {
+		// on déclare le nombre de note à 0 
+		double somme =0;
+		
+		//on déclare et initialise arrayliste
+		ArrayList<Double> maListe = new ArrayList<Double>();
+		Scanner Question = new Scanner(System.in);
+		//on défini la réponse sur n par defaut
+		String Response2="n";
+	
+		//on fait
+		do {
+			System.out.println("entrer une note");
+			 double Response = Question.nextDouble();
+			System.out.println("Souhaitez vous encore ajouter une réponse entrer o/n ?");
+			 Response2 = Question.next();
+			maListe.add(Response);
+			//on boucle tant que response2 est égal à o
+			} while("o".equals(Response2));
+		
+		//on additionne les notes
+		for(int i=0;i<maListe.size();i++)
+		{
+			 somme += maListe.get(i);
+		}
+		  //on affiche la somme des notes
+		  System.out.println("la moyenne est "+somme/maListe.size());
+		  
+		  //on récupére les notes
+		   for (double number : maListe) {
+		         System.out.println("la liste des notes  " + number);
+		      }
+	}
+
+}
