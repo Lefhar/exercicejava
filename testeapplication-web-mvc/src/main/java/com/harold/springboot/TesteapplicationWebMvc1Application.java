52f2e0d13b4e201b3c429bf1a2bd11b6797a083d
Lefebvre Harold <73162352+Lefhar@users.noreply.github.com>
2022-09-19 20:59:14 +0200

    exercice java

--- testeapplication-web-mvc/src/main/java/com/harold/springboot/TesteapplicationWebMvc1Application.java
+++ testeapplication-web-mvc/src/main/java/com/harold/springboot/TesteapplicationWebMvc1Application.java
@@ -1,9 +1,12 @@
 package com.harold.springboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = { ErrorMvcAutoConfiguration.class })
 public class TesteapplicationWebMvc1Application {
 
 	public static void main(String[] args) {
--- testeapplication-web-mvc/src/main/java/com/harold/springboot/service/EmployeeServiceImpl.java
+++ testeapplication-web-mvc/src/main/java/com/harold/springboot/service/EmployeeServiceImpl.java
@@ -34,7 +34,8 @@ public class EmployeeServiceImpl implements EmployeeService {
 		if (optional.isPresent()) {
 			employee = optional.get();
 		} else {
-			throw new RuntimeException(" Employee not found for id :: " + id);
+			// throw new RuntimeException(" Employee not found for id :: " + id);
+			// employee = " Employee not found for id :: " + id;
 		}
 		return employee;
 	}
